a472725957b4c5c4dc50982bb513aa42a738a99e
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-03-10 19:28:32 +0200

    fixing some bug

--- reunion.php
+++ reunion.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link rel="stylesheet" href="css/reunione.css">
+    <link rel="stylesheet" href="css/reunion.css">
     <title>Reunion & Evenements|HvH</title>
 </head>
 <body>
